73379f234c8c80552fde2cda75ef632a3b884a19
ayush2342 <ayushsharma2342@gmail.com>
2023-02-09 19:06:22 +0530

    Scroll height modified

--- Percentage Scrolled/script.js
+++ Percentage Scrolled/script.js
@@ -3,7 +3,11 @@ var maxHeight=window.innerHeight;
 var percentage=document.getElementById("percentageIndicator");
 window.addEventListener("scroll",function()
 {
-count=Math.floor(((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100));
+    var screenScrollTop=document.documentElement.scrollTop; //Distance of root element scrolled from top
+    var screenScrollHeight=document.documentElement.scrollHeight; //Total scrollabe Height of the document
+    var screenHeight=this.window.innerHeight; //Total height of the document
+    
+    count=Math.floor(((screenScrollTop)/(screenScrollHeight-screenHeight))*100);
 
     percentage.innerHTML=count+"% Viewed";
 })
